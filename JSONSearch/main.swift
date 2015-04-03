7193b727c57287458bf3ceaf90045f3a732ea602
Jeremy Pereira <jeremyp@jeremyp.net>
2015-04-03 13:28:35 +0100

    Initial Commit

---
+++ JSONSearch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JSONSearch
+//
+//  Created by Jeremy Pereira on 03/04/2015.
+//  Copyright (c) 2015 Jeremy Pereira. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
