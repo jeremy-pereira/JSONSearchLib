4ce7154995a95976783d1734533ea6b13740a73f
Jeremy Pereira <jeremyp@jeremyp.net>
2015-04-05 08:56:29 +0100

    Put something in a performance test

--- JSONSearchLibTests/TestJSONSearch.swift
+++ JSONSearchLibTests/TestJSONSearch.swift
@@ -81,8 +81,9 @@ class TestJSONSearch: XCTestCase
     func testPerformanceExample()
     {
         // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+        self.measureBlock()
+        {
+            theJSON.search("fields", recursive: true).search(0)
         }
     }
 
